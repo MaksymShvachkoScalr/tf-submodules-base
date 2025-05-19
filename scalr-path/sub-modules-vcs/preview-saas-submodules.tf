2137c881e1bb097ab238b05f3b564e6136c534d3
Heshercat <alfiiaqa@gmail.com>
2025-05-19 08:57:06 +0300

    Add tev2 submodules

---
+++ scalr-path/sub-modules-vcs/preview-saas-submodules.tf
@@ -0,0 +1,4 @@
+module "base_tf-module-guitar" {
+  source  = "heshy.alfi-master.testenv.scalr.dev/acc-v0oqh8bla0u5tn1r0/base/submodules//modules/tf-module-guitar"
+  version = "3.0.0"
+}
--- scalr-path/sub-modules-vcs/staging-submodules.tf
+++ scalr-path/sub-modules-vcs/staging-submodules.tf
@@ -1,4 +1,4 @@
-module "base_tf-module-drums" {
-  source  = "bluesky.main.scalr.dev/acc-v0o6dnihvv3lg7eq2/base/submodules//modules/tf-module-drums"
-  version = "0.1.4"
-}
+#module "base_tf-module-drums" {
+#  source  = "bluesky.main.scalr.dev/acc-v0o6dnihvv3lg7eq2/base/submodules//modules/tf-module-drums"
+#  version = "0.1.4"
+#}
